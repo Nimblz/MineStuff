553786557cbf6c220d81afd2ed52f02d2a1efd13
Nim <areuschle1@gmail.com>
2018-11-28 03:15:06 -0600

    Implemented pizzaalpaca modules

--- src/server/Game.server.lua
+++ src/server/Game.server.lua
@@ -8,10 +8,12 @@ local CommonModules = Common:WaitForChild("module")
 local Lib = ReplicatedStorage:WaitForChild("lib")
 local PizzaAlpaca = Lib:WaitForChild("pizzaalpaca")
 
+local PizzaAlpacaModules = PizzaAlpaca:WaitForChild("module")
 local SidedModules = script.Parent:WaitForChild("module")
 
 local ModuleManager = require(PizzaAlpaca.object.ModuleManager).new(PRINT_DEBUG)
 
+ModuleManager:AddModuleDirectory(PizzaAlpacaModules)
 ModuleManager:AddModuleDirectory(CommonModules)
 ModuleManager:AddModuleDirectory(SidedModules)
 
