4c883117e4ab3e994e68b40fc687f3b6877e8a08
Nim <areuschle1@gmail.com>
2019-02-04 23:37:31 -0600

    chunk now keeps track of its own position

--- src/common/object/Chunk.lua
+++ src/common/object/Chunk.lua
@@ -1,15 +1,18 @@
-local Settings = require(script.Parent.GameSettings)
+-- Object used to hold chunk data, kept track of by servermine, passed to rasterizer.
+
+local Settings = require(script.Parent.Parent.GameSettings)
 local CHUNK_SIZE = Settings.CHUNK_SIZE
 
 local Chunk = {}
 
 local function GetFlatIndex(x,y,z)
-    return x + (y * CHUNK_SIZE.WIDTH) + (z * CHUNK_SIZE.X * CHUNK_SIZE.Z)
+    return x + (y * CHUNK_SIZE.X) + (z * CHUNK_SIZE.X * CHUNK_SIZE.Z)
 end
 
-function Chunk.new()
+function Chunk.new(cx,cy,cz)
     local self = setmetatable({},{__index = Chunk})
 
+    self.ChunkPos = Vector3.new(cx,cy,cz)
     self.ChunkData = {}
 
     return self
