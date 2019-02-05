cb9312fd12da47fa09883079e5815e4293be3896
Nim <areuschle1@gmail.com>
2019-02-04 23:37:47 -0600

    new BLOCK_SIZE setting

--- src/common/GameSettings.lua
+++ src/common/GameSettings.lua
@@ -1,6 +1,7 @@
 local GameSettings = {
-    CHUNK_SIZE = Vector3int16.new(16,16,16),
-    MINE_CHUNK_LIMIT = 10000, -- Number of chunks that may exist before the mine colapses
+    CHUNK_SIZE = Vector3.new(16,16,16),
+    BLOCK_SIZE = Vector3.new(3,3,3),
+    MINE_CHUNK_LIMIT = 2000, -- Number of chunks that may exist before the mine colapses
 }
 
 return GameSettings
